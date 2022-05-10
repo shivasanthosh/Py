aaa830ae77804f0a4db9616a88b35f30f06b97fa
shiva <shivasanthosh@sysortech.com>
2022-05-10 17:59:50 +0530

    Added spiral matrix

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
-print("hello")
+def generateMatrix(n):
+    # filling it with 0's
+    matrix = [[0 for _ in range(n)] for _ in range(n)]
+    dirr = 0
+    row, col = 0, 0
+    num = 1
+    k = 1
+
+    while k <= n*n:
+        matrix[row][col] = k
+        k += 1
+        if dirr == 0:
+            col = col + 1
+            if col == n or matrix[row][col] != 0:
+                dirr = 1
+                col -= 1
+                row += 1
+        elif dirr == 1:
+            row = row + 1
+            if row == n or matrix[row][col] != 0:
+                dirr = 2
+                col -= 1
+                row -= 1
+        elif dirr == 2:
+            col = col - 1
+            if col < 0 or matrix[row][col] != 0:
+                dirr = 3
+                col += 1
+                row -= 1
+        elif dirr == 3:
+            row = row - 1
+            if row < 0 or matrix[row][col] != 0:
+                dirr = 0
+                col += 1
+                row += 1
+
+    return matrix
+
+
+
+
+
+
+
+print(generateMatrix(6))
+
+
