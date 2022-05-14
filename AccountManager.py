c9999a5a4ed822a016259b61af685a02d8c25a47
shiva <shivasanthosh@sysortech.com>
2022-05-14 13:14:56 +0530

    AccountManager ran successfully used queues to solve the issue

--- AccountManager.py
+++ AccountManager.py
@@ -1,28 +1,33 @@
 #Availve service
-A = 4
+A = 1
 #microsecond delay
-B = 5
+B = 3
 #orders
-C = [0, 1, 6, 11]
+C = [1, 4, 5, 9, 10, 14, 15, 18, 19, 20]
 
-
+# 5 6 11 16
+# 3 2 3 3
+def isNegative(D):
+    if D < 0:
+        return 0
+    return D
 def solve(A,B,C):
-    service = [0 for _ in range(A)]
+    l = len(C)
+    container = []
+    avaiable = [0 for _ in range(l)]
 
-    return service
-
-
-
-
-
-
-
-
-
-
+    for i in range(l):
+        container.append(C[i]+B)
+        for j in range(len(container)-1, -1, -1):
+            if container[j] <= C[i]:
+                avaiable[i] = A - len(container)
+                container.pop(0)
+        avaiable[i] = isNegative(A - len(container))
 
 
 
+
+    return avaiable
 
 print(solve(A,B,C))
 
